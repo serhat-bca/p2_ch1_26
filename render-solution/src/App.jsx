937d45131184d7c72464a19ce9c254a19153ed01
serhat-bca <sersen@bergen.org>
2026-04-28 09:37:39 -0400

    arrow function and components

--- render-solution/src/App.jsx
+++ render-solution/src/App.jsx
@@ -1,5 +1,16 @@
 import { useState } from "react";
 
+const Counter = ({ countVar }) => {
+  return (
+    <div>
+      <p>Counter: {countVar}</p>
+    </div>
+  );
+};
+
+// short version if you just want to return a simple thing
+// const Counter = ({ countVar }) => <p>Counter: {countVar}</p>;
+
 const App = () => {
   const [counter, setCounter] = useState(0);
 
@@ -10,11 +21,23 @@ const App = () => {
     setCounter(counter + 1);
   };
 
+  const updateCounter = (count) => {
+    setCounter(counter + count);
+  };
+
+  const resetCounter = () => {
+    setCounter(0);
+  };
+
   return (
     <div>
       <h2>Render Solution with React Hooks</h2>
-      <p>Counter: {counter}</p>
+      <Counter countVar={counter} />
       <button onClick={increaseCounter}>+</button>
+      <button onClick={() => updateCounter(5)}>+5</button>
+      <button onClick={resetCounter}>Reset</button>
+      <button onClick={() => updateCounter(-1)}>-</button>
+      <button onClick={() => updateCounter(-5)}>-5</button>
     </div>
   );
 };
